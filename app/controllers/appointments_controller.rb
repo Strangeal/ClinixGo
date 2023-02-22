40564a454c6db06ed6088e0eb156d9225970a91c
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-22 14:07:01 +0000

    Create index to get all appointments of a user

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,8 @@
 class AppointmentsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    appointments = user.appointments
+    render json: appointments, status: :ok
+  end
+
 end
