2b11161206da7530fecf35bea092aa1f22d94aa0
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-22 13:30:42 +0000

    Validate attributes of the Appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+
+  validates :appointment_date, presence: true, date: { after_or_equal_to: Date.today }
+  validates :start_time, presence: true, time: { after_or_equal_to: "08:00" }
+  validates :end_time, presence: true, time: { after_or_equal_to: "17:00" }
+  validates :user, presence: true
+  validates :doctor, presence: true
 end
