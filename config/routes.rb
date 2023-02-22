42fc402732dc7fdb1cb2ff5580d8ead62c03c601
strangeal <bajerijustice@gmail.com>
2023-02-22 04:48:38 -0800

    Add routes for doctors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'root#index'
-  resource :doctors
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resource :doctors
+
 end
