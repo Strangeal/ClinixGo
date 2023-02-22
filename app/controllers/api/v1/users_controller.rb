96307a20d543342e7b533de9fe151b663967bcf7
strangeal <bajerijustice@gmail.com>
2023-02-22 09:34:39 -0800

    Add create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      render json: {
+        data: @user, message: 'User created'
+      }, status: :created
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity, message: 'Sorry! Something went wrong'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age, :gender)
+  end
 end
