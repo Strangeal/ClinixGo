b2b7f8bcd7923541c7fd9fdc6d7ef5d626964721
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-22 16:20:18 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :doctors
-        resources :appointments
+        resources :doctors do
+          resources :appointments
+        end
       end
     end
   end
