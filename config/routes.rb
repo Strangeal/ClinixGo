fdbe54377abfa1cf2848f8f21a8fe8a742cf9504
strangeal <bajerijustice@gmail.com>
2023-02-22 09:34:56 -0800

    Update routes for doctors

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :doctors, only: [:index]
       resources :users do
         resources :doctors do
           resources :appointments
