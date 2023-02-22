8541b0c68e36586f2a080bb25264ee2c18ce003c
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-22 12:12:59 +0000

    Add content in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create 10 users
+10.times do 
+  users = User.create(name: "Adel", gender: "male", age: 25)
+end
