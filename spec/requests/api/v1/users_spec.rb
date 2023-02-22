8fcab522edcbebe41db1a6c850e2a3d1bd206acb
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-22 12:22:04 +0000

    Add test for the api response

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users", type: :request do
- 
+RSpec.describe 'Api::V1::Users', type: :request do
+
+  describe 'GET /index' do
+    it 'returns http success' do
+      get api_v1_users_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
