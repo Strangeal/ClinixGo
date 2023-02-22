edae676464cfed884c0743b7219ef7e80a467533
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-22 14:07:30 +0000

    Add action to create an appointment

--- db/migrate/20230222131607_create_appointments.rb
+++ db/migrate/20230222131607_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration[7.0]
   def change
     create_table :appointments do |t|
-      t.date :date
+      t.date :appointment_date
       t.time :start_time
       t.time :end_time
       t.references :user, null: false, foreign_key: true
