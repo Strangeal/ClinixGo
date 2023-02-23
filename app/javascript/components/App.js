8d0c70b27ce5d7984db2114cf6986f016b1c3e41
strangeal <bajerijustice@gmail.com>
2023-02-23 00:41:18 -0800

    Add app.js components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Demo from "./Demo";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+
+      <Routes>
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
