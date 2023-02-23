e1bc5bcf024fd1993c7a617f5fd526d1583aab87
strangeal <bajerijustice@gmail.com>
2023-02-23 01:24:25 -0800

    Add bootstrap

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import '../styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Demo from './Demo';
 
 const App = () => (
   <div>
-    <h1>Hello World!</h1>
+    <h1 className="text-primary">Hello World!</h1>
 
     <Routes>
       <Route path="/demo" element={<Demo />} />
