ef5d9e80dfe72b606e576168890a2508f3ed73fa
strangeal <bajerijustice@gmail.com>
2023-02-24 02:22:31 -0800

    Add reducers in store

--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
+import appointmentReducer from './appointments/appointmentSlice';
+import doctorIdReducer from './doctors/doctorIdSlice';
+import doctorReducer from './doctors/doctorSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    doctors: doctorReducer,
+    doctorsId: doctorIdReducer,
+    appointments: appointmentReducer
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
