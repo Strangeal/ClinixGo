abb8ea3242cb1becf02d8a82b0d6590f1c68fe5c
strangeal <bajerijustice@gmail.com>
2023-02-24 02:13:37 -0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :doctors, only: [:index]
-      resources :users do
-        resources :doctors do
+      resources :doctors, only: [:index, :show]
+      resources :users
           resources :appointments
-        end
-      end
     end
   end
   get '*path', to: 'root#index', via: :all
