bec599cff17d805de363669117f14fa5dfb0ea67
strangeal <bajerijustice@gmail.com>
2023-02-24 14:56:41 -0800

    Import Navbar component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,17 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
 import Demo from './Demo';
 import Details from './Details';
+import Navbar from './Navbar';
 import NotFound from './NotFound';
 
 const App = () => (
   <div>
     {/* <h1 className="text-primary">Hello World!</h1> */}
-
+    <Navbar />
+    <div>
     <Routes>
       <Route path="/demo" element={<Demo />} />
       <Route path="/details" element={<Details />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    </div>
   </div>
 );
 
