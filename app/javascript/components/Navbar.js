1979dc81f143215795e7a9c1fbde42184045e5fa
strangeal <bajerijustice@gmail.com>
2023-02-25 07:07:39 -0800

    Add nav element

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -6,10 +6,10 @@ import { BsFillJournalBookmarkFill } from 'react-icons/bs';
 import '../styles/navbar.css';
 
 const Navbar = () => (
-  <nav>
-    <div className="container-fluid">
+  <nav className="nav-bg-color">
+    <div className="container-fluid px-0">
       <div className="row flex-nowrap">
-        <div className="col-auto position-fixed col-md-3 col-xl-3 px-sm-2 px-0 nav-bg-color">
+        <div className="col-auto col-md-3 col-xl-3 px-sm-2 px-0">
           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
             <NavLink to="/demo" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white">
               <span className="fs-3 d-none d-sm-inline nav-brand-color">ClinixGo</span>
