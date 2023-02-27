44d7ba367fe320c11fb125c8d51ab1a6de793cda
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-27 16:01:00 +0000

    Give Navbar a position of fixed

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,28 +3,26 @@ import { Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
 import Demo from './Demo';
 import Details from './Details';
+import Homepage from './Homepage';
 import Navbar from './Navbar';
 import NotFound from './NotFound';
-import Homepage from './Homepage';
 
 const App = () => (
   <section className="container-fluid px-0">
     <div className="row">
-      <div className="col-3">
-          <Navbar />
+      <div className="col-2">
+        <Navbar />
       </div>
-      <div className="col-9">
+      <div className="col-10">
         <Routes>
           <Route path="/" element={<Homepage />} />
-      <Route path="/demo" element={<Demo />} />
+          <Route path="/demo" element={<Demo />} />
           <Route path="/details/:id" element={<Details />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </div>
   </section>
-      
-    
 );
 
 export default App;
--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { IoMdContacts } from 'react-icons/io';
-import { FaHome } from 'react-icons/fa';
 import { BsFillJournalBookmarkFill } from 'react-icons/bs';
+import { CgProfile } from 'react-icons/cg';
+import { FaHome } from 'react-icons/fa';
+import { IoMdContacts } from 'react-icons/io';
+import { Link, NavLink } from 'react-router-dom';
 import '../styles/navbar.css';
-import { CgProfile } from 'react-icons/cg'
 
 const Navbar = () => (
-  <nav className="nav-bg-color">
+  <nav className="nav-bg-color position-fixed">
     <div className="container-fluid px-0">
       <div className="row flex-nowrap">
         <div className="col-auto col-md-3 col-xl-3 mx-sm-auto mx-md-0">
-          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <NavLink to="/demo" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white">
-              <span className="fs-5 d-none d-md-inline nav-brand-color">ClinixGo</span>
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white vh-100">
+            <NavLink
+              to="/demo"
+              className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white"
+            >
+              <span className="fs-5 d-none d-md-inline nav-brand-color">
+                ClinixGo
+              </span>
             </NavLink>
             <ul className="nav flex-column md-sm-auto mb-0 align-items-center align-items-sm-start">
               <li className="nav-item pb-2">
                 <NavLink to="/" className="nav-link align-middle px-0">
                   <FaHome className="fs-4 nav-icon" />
-                  <span className="sm-1 d-none px-md-2 d-md-inline">Doctors</span>
+                  <span className="sm-1 d-none px-md-2 d-md-inline">
+                    Doctors
+                  </span>
                 </NavLink>
               </li>
               <li className="nav-item pb-2">
-                <NavLink to="/details" className="nav-link align-middle px-0 d-flex p-0">
+                <NavLink
+                  to="/appointments"
+                  className="nav-link align-middle px-0 d-flex p-0"
+                >
                   <BsFillJournalBookmarkFill className="fs-4 nav-icon" />
-                  <span className="sm-1 d-none px-md-2 d-md-inline">Appointments</span>
+                  <span className="sm-1 d-none px-md-2 d-md-inline">
+                    Appointments
+                  </span>
                 </NavLink>
               </li>
               <li className="nav-item pb-2">
@@ -34,23 +46,43 @@ const Navbar = () => (
                   <span className="sm-1 d-none px-md-2 d-md-inline">About</span>
                 </NavLink>
               </li>
-            </ul>        
+            </ul>
 
-              {/* Dropdown profile */}
-              <div className="dropdown pb-4 dropdown-nav">
-                <Link className="d-flex align-items-center align-middle px-0 text-white dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                  <CgProfile className="fs-3 nav-icon" />
-                  <span className="current_username d-none d-md-inline mx-1">Username</span>
-                </Link>
-
-                <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser">
-                  <li><Link className="dropdown-item">New Appointment</Link></li>
-                  <li><Link className="dropdown-item">Profile</Link></li>
-                  <li> <hr className="dropdown-divider" /> </li>
-                  <li><Link className="dropdown-item">Sign out</Link></li>
-                </ul>
+            {/* Dropdown profile */}
+            <div className="dropdown pb-4 dropdown-nav">
+              <div
+                className="d-flex align-items-center align-middle px-0 text-white dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style={{ cursor: 'pointer' }}
+              >
+                <CgProfile className="fs-3 nav-icon" />
+                <span className="current_username d-none d-md-inline mx-1">
+                  Username
+                </span>
               </div>
 
+              <ul
+                className="dropdown-menu dropdown-menu-dark text-small shadow"
+                aria-labelledby="dropdownUser"
+              >
+                <li>
+                  <Link to="/new_appointment" className="dropdown-item">
+                    New Appointment
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item">Profile</Link>
+                </li>
+                <li>
+                  {' '}
+                  <hr className="dropdown-divider" />{' '}
+                </li>
+                <li>
+                  <Link className="dropdown-item">Sign out</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
