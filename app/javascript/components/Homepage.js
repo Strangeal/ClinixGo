80b43aa966cb98a80b8515550792deefa62fdfa6
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-27 14:23:18 +0000

    Update the Homepage styling

--- app/javascript/components/Homepage.js
+++ app/javascript/components/Homepage.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchDocData } from "../redux/doctors/doctorSlice";
-import "../styles/Home.css";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchDocData } from '../redux/doctors/doctorSlice';
+import '../styles/Home.css';
 
 const Homepage = () => {
   const dispatch = useDispatch();
@@ -11,37 +10,38 @@ const Homepage = () => {
 
   useEffect(() => {
     dispatch(fetchDocData());
-   }, [dispatch]);
+  }, [dispatch]);
 
   return (
-    <section className="container mt-4 main-container">
-      <div >
-      <h1 className="text-center">Oncall Doctors</h1>
-      <p className="text-center sub-header">Please choose a specialist</p>
+    <section className="mt-4 main-container">
+      <div>
+        <h1 className="text-center">Oncall Doctors</h1>
+        <p className="text-center sub-header">Please choose a specialist</p>
       </div>
 
       <p className="text-center dotted mb-5">................</p>
 
       <main className="doctors-container">
         {doctorsList.map((doctor) => (
-        <div className="mb-5" key={doctor.id}>
-          <div className="container">
-            <span className="dot" />
-            <img
-              src={doctor.photo}
-              className="mx-auto d-block img-fluid"
-              alt="doctor"
-            />
-              <h3 className="text-center mt-3">{doctor.name }</h3>
-          </div>
+          <div className="mb-5" key={doctor.id}>
+            <div className="home-img-container">
+              {/* <span className="dot" /> */}
+              <img src={doctor.photo} alt="doctor" />
+            </div>
+            <h3 className="text-center mt-3">{doctor.name}</h3>
 
-          <div className="bottom-container text-center">
-            <span className="dotted">................</span>
-            <p className="btm-text">
-              {doctor.bio}
-            </p>
-            <Link to={`details/${doctor.id}`} type="button" className="btn btn-outline-secondary"> View more</Link>
-          </div>
+            <div className="bottom-container text-center">
+              <span className="dotted">................</span>
+              <p className="btm-text">{doctor.bio}</p>
+              <Link
+                to={`details/${doctor.id}`}
+                type="button"
+                className="btn btn-outline-success"
+              >
+                {' '}
+                View more
+              </Link>
+            </div>
           </div>
         ))}
       </main>
