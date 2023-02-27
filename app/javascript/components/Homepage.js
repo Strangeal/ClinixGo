5db8159222a3795630f3deb971e5c742ce8fcb40
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-27 12:18:57 +0000

    Consume the doctors API and remove static data

--- app/javascript/components/Homepage.js
+++ app/javascript/components/Homepage.js
@@ -1,82 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchDocData } from "../redux/doctors/doctorSlice";
 import "../styles/Home.css";
 
-const Homepage = () => (
-  <section className="container mt-4">
-    <h1 className="text-center">Oncall Doctors</h1>
-    <p className="text-center sub-header">Please choose a specialist</p>
+const Homepage = () => {
+  const dispatch = useDispatch();
+  const doctorsList = useSelector((state) => state.doctors.doctors);
 
-    <main className="doctors-container">
-      <div className="mb-5">
-        <div className="container">
-          <span className="dot" />
-          <img
-            src="https://res.cloudinary.com/db3ckadxp/image/upload/v1677239506/doc1_rzintp.png"
-            className="mx-auto d-block img-fluid"
-            alt="doctor"
-          />
-          <h3 className="text-center mt-3">Hannah Dosie</h3>
-        </div>
+  useEffect(() => {
+    dispatch(fetchDocData());
+   }, [dispatch]);
 
-        <div className="bottom-container text-center">
-          <span className="dotted">................</span>
-          <p className="btm-text">
-            Lorem ipsum text asaj abs ye byeabde eybddbye aey sixe nqoqnud
-            qoqnsqj Q;WDwqun
-          </p>
-          <button type="button" className="btn btn-outline-secondary">
-            View more
-          </button>
-        </div>
-      </div>
+  return (
+    <section className="container mt-4">
+      <h1 className="text-center">Oncall Doctors</h1>
+      <p className="text-center sub-header">Please choose a specialist</p>
 
-      <div className="mb-5">
-        <div className="container">
-          <span className="dot" />
-          <img
-            src="https://res.cloudinary.com/db3ckadxp/image/upload/v1677239506/doc1_rzintp.png"
-            className="mx-auto d-block img-fluid"
-            alt="doctor"
-          />
-          <h3 className="text-center mt-3">Sam Hassan</h3>
-        </div>
+      <main className="doctors-container">
+        {doctorsList.map((doctor) => (
+        <div className="mb-5" key={doctor.id}>
+          <div className="container">
+            <span className="dot" />
+            <img
+              src={doctor.photo}
+              className="mx-auto d-block img-fluid"
+              alt="doctor"
+            />
+              <h3 className="text-center mt-3">{doctor.name }</h3>
+          </div>
 
-        <div className="bottom-container text-center">
-          <span className="dotted">................</span>
-          <p className="btm-text">
-            Lorem ipsum text asaj abs ye byeabde eybddbye aey sixe nqoqnud
-            qoqnsqj Q;WDwqun
-          </p>
-          <button type="button" className="btn btn-outline-secondary">
-            View more
-          </button>
-        </div>
-      </div>
-
-      <div className="mb-5">
-        <div className="container">
-          <span className="dot" />
-          <img
-            src="https://res.cloudinary.com/db3ckadxp/image/upload/v1677239506/doc1_rzintp.png"
-            className="mx-auto d-block img-fluid"
-            alt="doctor"
-          />
-          <h3 className="text-center mt-3">Mille Paul</h3>
-        </div>
-
-        <div className="bottom-container text-center">
-          <span className="dotted">................</span>
-          <p className="btm-text">
-            Lorem ipsum text asaj abs ye byeabde eybddbye aey sixe nqoqnud
-            qoqnsqj Q;WDwqun
-          </p>
-          <button type="button" className="btn btn-outline-secondary">
-            View more
-          </button>
-        </div>
-      </div>
-    </main>
-  </section>
-);
+          <div className="bottom-container text-center">
+            <span className="dotted">................</span>
+            <p className="btm-text">
+              {doctor.bio}
+            </p>
+            <Link to={`details/${doctor.id}`} type="button" className="btn btn-outline-secondary"> View more</Link>
+          </div>
+          </div>
+        ))}
+      </main>
+    </section>
+  );
+};
 
 export default Homepage;
