ec7a8d75766ec1f4d44420d56f527d5aaaa44db4
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-28 17:00:02 +0000

    Validate fields

--- app/javascript/components/add-doctor/AddDoctor.js
+++ app/javascript/components/add-doctor/AddDoctor.js
@@ -4,6 +4,8 @@ import moment from 'moment';
 import React, { useState } from 'react';
 import * as Yup from 'yup';
 import '../../styles/AddDoc.css';
+import Availability from './Availability';
+import PersonalInfo from './PersonalInfo';
 const initialValues = {
   name: '',
   email: '',
@@ -19,6 +21,35 @@ const initialValues = {
   ratings: 0,
   patients: 0,
 };
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .min(3, 'must be at least 3 characters')
+    .required('Name is required'),
+  bio: Yup.string()
+    .min(5, 'must be at least 3 characters')
+    .required('Bio is required'),
+  photo: Yup.string().required('Photo is required'),
+  available_days: Yup.string().required('Available days is required'),
+  specialities: Yup.string().required('Specialities is required'),
+  hospital: Yup.string().required('Hospital name is required'),
+  start_time: Yup.string().required('Start time is required'),
+  end_time: Yup.string()
+    .required('End time is required')
+    .test('is-greater', 'End Time must be greater', (value) => {
+      // eslint-disable-next-line camelcase
+      const { start_time } = this.parent;
+      return moment(value, 'HH:mm').isSameOrAfter(moment(start_time, 'HH:mm'));
+    }),
+  email: Yup.string()
+    .email('Must be a valid email address')
+    .required('Email is required'),
+  phone: Yup.string().required('Phone is required'),
+  reviews: Yup.number().required('Reviews is required'),
+  ratings: Yup.number().required('Ratings is required'),
+  patients: Yup.number().required('Patients is required'),
+});
+
 const AddDoctor = () => {
     <section
       id="add-doctor"
