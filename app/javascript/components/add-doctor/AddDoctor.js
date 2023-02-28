7e45d459e4c90f54e611748afdc1823ed87612ac
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-28 16:58:27 +0000

    Create component to add a doctor

---
+++ app/javascript/components/add-doctor/AddDoctor.js
@@ -0,0 +1,13 @@
+const AddDoctor = () => {
+    <section
+      id="add-doctor"
+      className="py-5 vh-lg-100 d-flex flex-column justify-content-center"
+    >
+      <h1 className="mt-1 d-none d-lg-block text-center mt-lg-5">
+        Add a Doctor
+      </h1>
+    </section>
+  );
+};
+
+export default AddDoctor;
