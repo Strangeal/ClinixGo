d61a93c0469b42592e4f5e0586e828b535fbd597
Paul-js-hub <hesbonpoisson@gmail.com>
2023-03-01 07:54:51 +0300

    Fix the toastify error

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/App.css';
 import Demo from './Demo';
 import Details from './Details';
