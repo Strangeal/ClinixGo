8eca24a50137179f689906b9bd98940b648dc360
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-01 20:56:40 +0000

    Add method to create a doctor

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -9,4 +9,21 @@ class Api::V1::DoctorsController < ApplicationController
     @doctor = Doctor.find(params[:id])
     render json: @doctor, status: :ok
   end
+
+  def create
+    @doctor = Doctor.create(doctor_params)
+    if @doctor.save
+      render json: {
+        message: 'Created'
+      }, status: :created
+    else
+      render json: @doctor.errors.full_messages, status: :unprocessable_entity, message: 'Sorry! Something went wrong'
+    end
+  end
+
+  private
+  
+  def doctor_params
+    params.require(:doctor).permit(:name, :bio, :photo, :available_days, :specialities, :hospital, :start_time, :end_time, :email, :phone, :reviews, :experience, :rating, :patients)
+  end
 end
