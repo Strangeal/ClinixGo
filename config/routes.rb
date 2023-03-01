6ebf8215d1239aaf3edd8410f7a230fa55ae940b
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-01 21:05:55 +0000

    Add the create route for a doctor

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :doctors, only: [:index, :show]
+      resources :doctors, only: [:index, :show, :create]
       resources :users
       resources :appointments
       post "auth/login", to: "users#login"
