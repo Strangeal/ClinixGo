65b2d562fe347762fb6421c92b1725823d379a9b
nahnahAJ <nahnahahjuwah@gmail.com>
2023-03-02 03:53:48 +0000

    Add new dummy content to style page

--- app/javascript/components/AppointmentCards.js
+++ app/javascript/components/AppointmentCards.js
@@ -12,8 +12,39 @@ const AppointmentCards = () => {
       <h1 className="appointment-header">Appointments</h1>
         <h5><BsFillPersonFill className="app-icons" />Patient: Amir Khan</h5>
       </div>
-
+<div className="cards">
     <div className="container background-of-card">
+      <img src="https://res.cloudinary.com/db3ckadxp/image/upload/v1677239541/doc2_nv5ug5.png" alt="Avatar" className="card-image" />
+      <div className="content">
+      <span><BsFileEarmarkPerson className="app-icons"/><b>Doctor: </b>Cincinatti Pagne</span>
+      <span><BsFillCalendarDateFill className="app-icons"/><b>Date: </b>10th May 2023</span>
+      <span><BiTime className="app-icons"/><b>Time: </b>11:54am - 12:54pm</span>
+        <span><GrStatusCriticalSmall className="app-icons"/><b>Status: </b>pending</span>
+        </div>
+      </div>
+
+      <div className="container background-of-card">
+      <img src="https://res.cloudinary.com/dskl0qde4/image/upload/v1677227659/assets/rvzpkwlpfiwtntpdxezk.png" alt="Avatar" className="card-image" />
+      <div className="content">
+      <span><BsFileEarmarkPerson className="app-icons"/><b>Doctor: </b>Cincinatti Pagne</span>
+      <span><BsFillCalendarDateFill className="app-icons"/><b>Date: </b>10th May 2023</span>
+      <span><BiTime className="app-icons"/><b>Time: </b>11:54am - 12:54pm</span>
+        <span><GrStatusCriticalSmall className="app-icons"/><b>Status: </b>pending</span>
+        </div>
+      </div>
+
+      <div className="container background-of-card">
+      <img src="https://res.cloudinary.com/dskl0qde4/image/upload/v1677331291/assets/doc_2_pbbvzx.png" alt="Avatar" className="card-image" />
+      <div className="content">
+      <span><BsFileEarmarkPerson className="app-icons"/><b>Doctor: </b>Cincinatti Pagne</span>
+      <span><BsFillCalendarDateFill className="app-icons"/><b>Date: </b>10th May 2023</span>
+      <span><BiTime className="app-icons"/><b>Time: </b>11:54am - 12:54pm</span>
+        <span><GrStatusCriticalSmall className="app-icons"/><b>Status: </b>pending</span>
+        </div>
+      </div>
+      </div>
+
+      <div className="container background-of-card">
       <img src="https://res.cloudinary.com/db3ckadxp/image/upload/v1677239541/doc2_nv5ug5.png" alt="Avatar" className="card-image" />
       <div className="content">
       <span><BsFileEarmarkPerson className="app-icons"/><b>Doctor: </b>Cincinatti Pagne</span>
