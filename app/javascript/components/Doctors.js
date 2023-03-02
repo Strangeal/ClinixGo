f4e7eeb070699074a5a5bd3ee4816d732b64d85a
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-02 09:25:40 +0000

    Create Doctors component
    This component is only accessible to the admin to manage the doctors

---
+++ app/javascript/components/Doctors.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { TbTrashOff } from 'react-icons/tb';
+
+const Doctors = () => {
+  return (
+    <section className="container-fliud doc-section">
+      <h1>Doctors</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Doctors;
