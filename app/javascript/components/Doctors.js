037736351648fc1f14895c6d61e3ea9c29f8da0d
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-02 09:26:35 +0000

    Display the list of doctors

--- app/javascript/components/Doctors.js
+++ app/javascript/components/Doctors.js
@@ -1,10 +1,59 @@
 import React, { useEffect } from 'react';
 import { TbTrashOff } from 'react-icons/tb';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDocData } from '../redux/doctors/doctorSlice';
+import '../styles/doctors.css';
 
 const Doctors = () => {
+  const dispatch = useDispatch();
+  const { doctors } = useSelector((state) => state.doctors);
+  console.log(doctors);
+
+  useEffect(() => {
+    dispatch(fetchDocData());
+  }, [dispatch]);
+
   return (
     <section className="container-fliud doc-section">
       <h1>Doctors</h1>
+      <div className="doc_container border">
+        <table className="table">
+          <thead className="container">
+            <tr className="">
+              <th scope="col">Basic Info</th>
+              <th scope="col" className="doc-date">
+                Specialities
+              </th>
+              <th scope="col" className="doc-status">
+                Status
+              </th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {doctors.map((doctor) => (
+              <tr key={doctor.id}>
+                <td className="doc-profile">
+                  <img
+                    className="doc-img img-fluid bg-secondary"
+                    src={doctor.photo}
+                    alt=""
+                  />
+                  <div className="doc-content">
+                    <p className="name">{doctor.name}</p>
+                    <p className="contact text-muted">{doctor.email}</p>
+                  </div>
+                </td>
+                <td className="doc-date-data">{doctor.specialities}</td>
+                </td>
+
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="doc_profile" />
       </div>
     </section>
   );
