0608a9df68f9b76d602124efcda5af4e5b926706
strangeal <bajerijustice@gmail.com>
2023-03-04 03:02:23 -0800

    Update login component

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { loginUser, userSelector } from '../redux/user/userSlice';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { loginUser, userSelector } from '../redux/user/userSlice';
+import '../styles/login.css'
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -11,43 +12,49 @@ const Login = () => {
   const { error, errorMessage, message, success } = user.user;
   const [state, setState] = useState({
     username: '',
-    password: '',
+    password: ''
   });
 
   const handleChange = (e) => {
     setState({
       ...state,
-      [e.target.name]: e.target.value,
-    });
-  };
+      [e.target.name]: e.target.value
+    })
+  }
 
   const handleSubmit = () => {
     const { username, password } = state;
-    dispatch(
-      loginUser({
-        username,
-        password,
-      })
-    );
-  };
+    dispatch(loginUser({
+      username,
+      password
+    }))
+  }
 
   useEffect(() => {
     if (success) {
       toast.success(message);
-      navigate('/');
+      navigate("/appointment");
     }
 
     if (error) {
       toast.error(errorMessage);
     }
-  }, [error, errorMessage, success, message, navigate]);
+  }, [error, errorMessage, success, message, navigate])
   return (
-    <div className="container">
-      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-        <form className="space-y-6">
-          <div className="mb-3">
-            <label htmlFor="usernameFormControlInput" className="form-label">
-              Username
+    <section className="container-fluid log-section">
+      <div className="row log-container">
+
+        <div className="col-md-6">
+          <div className="login-content d-flex flex-column justify-content-center align-items-center">
+            <img className="log-img img-fluid" src="https://res.cloudinary.com/dxsom7jmx/image/upload/v1677768358/undraw_welcome_re_h3d9_e8aywj.svg" alt="" />
+          <h4>Log in</h4>
+          </div>
+        </div>
+
+        <div className="log-form col-md-6 d-flex flex-column justify-content-center">
+        <form className="d-flex flex-column justify-content-center align-items-center">
+          <div className="form-group mb-3">
+            <label htmlFor="usernameFormControlInput" className="form-label">Username </label>
               <input
                 type="text"
                 className="form-control"
@@ -57,11 +64,11 @@ const Login = () => {
                 value={state.username}
                 onChange={handleChange}
               />
-            </label>
+            
           </div>
-          <div className="mb-3">
-            <label htmlFor="passwordFormControlInput" className="form-label">
-              Password
+
+          <div className="form-group mb-3">
+            <label htmlFor="passwordFormControlInput" className="form-label">Password</label>
               <input
                 type="password"
                 className="form-control"
@@ -71,30 +78,26 @@ const Login = () => {
                 value={state.password}
                 onChange={handleChange}
               />
-            </label>
           </div>
-          <div className="mb-3">
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleSubmit}
-            >
-              Login
-            </button>
-          </div>
+
+            <button type="button" className="btn btn-success d-block w-100 rounded-pill" onClick={handleSubmit}>Login</button>
         </form>
+
+
         <div className="mt-6">
           <div className="relative">
-            <div className="relative d-flex justify-center text-sm">
+            <div className="relative d-flex flex-column justify-content-center align-items-center text-sm">
               <span className="px-2 bg-white text-gray-500">
                 <Link to="/register">Register</Link>
               </span>
             </div>
           </div>
         </div>
+
       </div>
-    </div>
-  );
-};
+      </div>
+    </section>
+  )
+}
 
 export default Login;
