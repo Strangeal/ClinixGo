1262abc9582d62abce2f76e740a6816dd0cf6d1b
strangeal <bajerijustice@gmail.com>
2023-03-04 03:02:42 -0800

    Update register component

--- app/javascript/components/Register.js
+++ app/javascript/components/Register.js
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { registerUser, userSelector } from '../redux/user/userSlice';
+import '../styles/login.css'
 const Register = () => {
   const [state, setState] = useState({
     name: '',
@@ -37,7 +38,7 @@ const Register = () => {
   useEffect(() => {
     if (success) {
       toast.success(message);
-      navigate('/appointments');
+      navigate('/appointment');
     }
 
     if (error) {
@@ -46,12 +47,22 @@ const Register = () => {
     }
   }, [error, errorMessage, success, message, navigate]);
   return (
-    <div className="container">
-      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-        <form className="space-y-6">
-          <div className="mb-3">
-            <label htmlFor="nameFormControlInput" className="form-label">
-              Name
+    <section className="container-fluid log-section">
+      <div className="row log-container">
+
+        <div className="col-md-6">
+          <div className="login-content d-flex flex-column justify-content-center align-items-center">
+          <img className="log-img img-fluid" src="https://res.cloudinary.com/dxsom7jmx/image/upload/v1677769327/undraw_access_account_re_8spm_pkskcs.svg" alt="" />
+          <h4>Register</h4>
+          </div>
+        </div>
+
+
+
+        <div className="log-form col-md-6 d-flex flex-column justify-content-center">
+        <form className="d-flex flex-column justify-content-center align-items-center">
+          <div className=" form-group mb-3">
+            <label htmlFor="nameFormControlInput" className="form-label">Name</label>
               <input
                 type="text"
                 className="form-control"
@@ -61,11 +72,9 @@ const Register = () => {
                 value={state.name}
                 onChange={handleChange}
               />
-            </label>
           </div>
-          <div className="mb-3">
-            <label htmlFor="emailFormControlInput" className="form-label">
-              Email address
+          <div className="form-group mb-3">
+            <label htmlFor="emailFormControlInput" className="form-label">Email address</label>
               <input
                 type="email"
                 className="form-control"
@@ -75,11 +84,10 @@ const Register = () => {
                 value={state.email}
                 onChange={handleChange}
               />
-            </label>
           </div>
-          <div className="mb-3">
-            <label htmlFor="usernameFormControlInput" className="form-label">
-              Username
+          <div className="form-group mb-3">
+            <label htmlFor="usernameFormControlInput" className="form-label">Username</label>
+              
               <input
                 type="text"
                 className="form-control"
@@ -89,11 +97,10 @@ const Register = () => {
                 value={state.username}
                 onChange={handleChange}
               />
-            </label>
+            
           </div>
-          <div className="mb-3">
-            <label htmlFor="passwordFormControlInput" className="form-label">
-              Password
+          <div className="form-group mb-3">
+            <label htmlFor="passwordFormControlInput" className="form-label">Password</label>
               <input
                 type="password"
                 className="form-control"
@@ -103,23 +110,23 @@ const Register = () => {
                 value={state.password}
                 onChange={handleChange}
               />
-            </label>
           </div>
-          <div className="mb-3">
-            <button type="button" className="btn btn-primary" onClick={handleSubmit}>Register</button>
-          </div>
+            <button type="button" className="btn btn-success d-block w-100 rounded-pill" onClick={handleSubmit}>Register</button>
         </form>
+
         <div className="mt-6">
           <div className="relative">
-            <div className="relative d-flex justify-center text-sm">
+            <div className="relative d-flex flex-column justify-center align-items-center text-sm">
               <span className="px-2 bg-white text-gray-500">
                 <Link to="/login">Login</Link>
               </span>
             </div>
           </div>
         </div>
+
       </div>
-    </div>
+     </div>
+    </section>
   )
 }
 
