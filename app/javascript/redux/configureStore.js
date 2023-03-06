4770de3ea43d78a3e0f80672d59fd797bea5feb8
strangeal <bajerijustice@gmail.com>
2023-03-06 03:03:22 -0800

    Add books appointment slice

--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,17 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { logger } from 'redux-logger';
-import appointmentReducer from './appointments/appointmentSlice';
-import createDocSlice from './doctors/createDocSlice';
-import doctorIdReducer from './doctors/doctorIdSlice';
-import doctorReducer from './doctors/doctorSlice';
-import userReducer from './user/userSlice';
-import updateDoctorStatusSlice from './doctors/updateDocStatusSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { logger } from "redux-logger";
+import appointmentReducer from "./appointments/appointmentSlice";
+import createDocSlice from "./doctors/createDocSlice";
+import doctorIdReducer from "./doctors/doctorIdSlice";
+import doctorReducer from "./doctors/doctorSlice";
+import userReducer from "./user/userSlice";
+import updateDoctorStatusSlice from "./doctors/updateDocStatusSlice";
+import bookAppointmentReducer from "./appointments/bookAppointmentSlice";
 
 const store = configureStore({
   reducer: {
     doctors: doctorReducer,
     doctorsId: doctorIdReducer,
     appointments: appointmentReducer,
+    bookAppointment: bookAppointmentReducer,
     user: userReducer,
     newDoctor: createDocSlice,
     updateDoctorStatusSlice,
