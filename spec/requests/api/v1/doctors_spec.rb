d7ba02b694820f4c0df383571ef74e59ff34650c
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-07 08:37:55 +0000

    Update doctors_spec.rb

--- spec/requests/api/v1/doctors_spec.rb
+++ spec/requests/api/v1/doctors_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe 'api/v1/doctors', type: :request do
   path '/api/v1/doctors' do
     get('list doctors') do
       response(200, 'successful') do
+        # def encode_token(payload, exp = 7.days.from_now)
+        #   payload[:exp] = exp.to_i
+        #   JWT.encode(payload, 'mysecret')
+        # end
+        # let(:user) do
+        #   User.create(name: 'John Smith', username: 'smithey', email: 'john@smithey.com', password: 'smithey123')
+        # end
+        # let(:auth_token) { encode_token({ user_id: user.id }) }
+        # let(:Authorization) { "Bearer #{auth_token}" }
+
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -47,19 +57,47 @@ RSpec.describe 'api/v1/doctors', type: :request do
         }
       }
 
-      response '201', 'doctor created' do
+      response 201, 'doctor created' do
         let(:doctor) do
-          @first_doc = Doctor.create(name: 'Mike Venom', bio: 'Lorem ipsum is placeholder',
-                                     photo: 'https://img.freepik.com/free-photo/world-doctors-day-thinking-blonde-young-doctor-with-stethoscope-lab-coat_140725-162409.jpg?', available_days: 'Moday - Frinday', specialities: 'Primary Care Doctor', hospital: 'NYU Langone Health', start_time: '08: 00 AM', end_time: '20: 00 PM', email: 'doc.mikevenom@apple.com', phone: 1_555_000_213, reviews: 235, experience: 4, rating: 4.8, patients: 25)
+          Doctor.create(name: 'Mike Venom', bio: 'Lorem ipsum is placeholder',
+                        photo: 'https://img.freepik.com/free-photo/world-doctors-day-thinking-blonde-young-doctor-with-stethoscope-lab-coat_140725-162409.jpg?', available_days: 'Moday - Frinday', specialities: 'Primary Care Doctor', hospital: 'NYU Langone Health', start_time: '08: 00 AM', end_time: '20: 00 PM', email: 'doc.mikevenom@apple.com', phone: '1550213', reviews: 235, experience: 4, rating: 4.8, patients: 25)
         end
+
+        def encode_token(payload, exp = 7.days.from_now)
+          payload[:exp] = exp.to_i
+          JWT.encode(payload, 'mysecret')
+        end
+
+        let(:user) do
+          User.create(name: 'John Smith', username: 'smithey', email: 'john@smithey.com', password: 'smithey123', role: 'admin')
+        end
+
+        let(:auth_token) { encode_token({ user_id: user.id }) }
+
+        let(:Authorization) { "Bearer #{auth_token}" }
+
         run_test!
       end
 
-      response '422', 'invalid request' do
+      response 422, 'invalid request' do
         let(:doctor) do
-          @first_doc = Doctor.create(name: 'Mike Venom', bio: 'Lorem ipsum is placeholder',
-                                     photo: 'https://img.freepik.com/free-photo/world-doctors-day-thinking-blonde-young-doctor-with-stethoscope-lab-coat_140725-162409.jpg?', available_days: 'Monday - Frinday', specialities: 'Primary Care Doctor', hospital: 'NYU Langone Health', start_time: '08: 00 AM', end_time: '20: 00 PM', email: 'doc.mikevenom@apple.com', phone: 1_555_000_213)
+          Doctor.create(name: 'Mike Venom', bio: 'Lorem ipsum is placeholder',
+                        photo: 'https://img.freepik.com/free-photo/world-doctors-day-thinking-blonde-young-doctor-with-stethoscope-lab-coat_140725-162409.jpg?', available_days: 'Monday - Frinday', specialities: 'Primary Care Doctor', hospital: 'NYU Langone Health', start_time: '08: 00 AM', end_time: '20: 00 PM', email: 'doc.mikevenom@apple.com', phone: '55500213')
         end
+
+        def encode_token(payload, exp = 7.days.from_now)
+          payload[:exp] = exp.to_i
+          JWT.encode(payload, 'mysecret')
+        end
+
+        let(:user) do
+          User.create(name: 'John Smith', username: 'smithey', email: 'john@smithey.com', password: 'smithey123', role: 'admin')
+        end
+
+        let(:auth_token) { encode_token({ user_id: user.id }) }
+
+        let(:Authorization) { "Bearer #{auth_token}" }
+
         run_test!
       end
     end
@@ -71,7 +109,12 @@ RSpec.describe 'api/v1/doctors', type: :request do
 
     get('show doctor') do
       response(200, 'successful') do
-        let(:id) { '123' }
+        let(:doctor) do
+          Doctor.create(name: 'Mike Venom', bio: 'Lorem ipsum is placeholder',
+                        photo: 'https://img.freepik.com/free-photo/world-doctors-day-thinking-blonde-young-doctor-with-stethoscope-lab-coat_140725-162409.jpg?', available_days: 'Monday - Frinday', specialities: 'Primary Care Doctor', hospital: 'NYU Langone Health', start_time: '08: 00 AM', end_time: '20: 00 PM', email: 'doc.mikevenom@apple.com', phone: '1555000213')
+        end
+
+        let(:id) { doctor.id }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -87,10 +130,9 @@ RSpec.describe 'api/v1/doctors', type: :request do
 
   path '/api/v1/doctors/{id}' do
     put('update doctor info') do
-      let(:id) { '123' }
       security [{ bearer_auth: [] }]
       consumes 'application/json'
-      parameter name: :id, in: :path, type: :string, description: 'doctors id'
+      parameter name: :id, in: :path, type: :string, description: 'id'
       parameter name: :Authorization, in: :header, type: :string, required: true, description: 'Client token'
       parameter name: :doctor, in: :body, schema: {
         type: :object,
@@ -105,19 +147,68 @@ RSpec.describe 'api/v1/doctors', type: :request do
         }
       }
 
-      response '201', 'doctor updated' do
-        let(:doctor) do
-          @first_doc = Doctor.update(active: true)
-        end
-        run_test!
-      end
+      # response '200', 'doctor updated' do
+      #   doc1 = Doctor.create(name: 'Mike Venom', bio: 'Lorem ipsum is placeholder',
+      #                        photo: 'https://img.freepik.com/free-photo/world-doctors-day-thinking-blonde-young-doctor-with-stethoscope-lab-coat_140725-162409.jpg?', available_days: 'Monday - Frinday', specialities: 'Primary Care Doctor', hospital: 'NYU Langone Health', start_time: '08: 00 AM', end_time: '20: 00 PM', email: 'doc.mikevenom@apple.com', phone: '1555000213', active: true)
 
-      response '422', 'invalid request' do
-        let(:doctor) do
-          @first_doc = Doctor.update(active: true)
-        end
-        run_test!
-      end
+      #   let(:id) { doc1.id }
+
+      #   def encode_token(payload, exp = 7.days.from_now)
+      #     payload[:exp] = exp.to_i
+      #     JWT.encode(payload, 'mysecret')
+      #   end
+
+      #   let(:user) do
+      #     User.create(name: 'John Smith', username: 'smithey', email: 'john@smithey.com', password: 'smithey123', role: 'admin')
+      #   end
+
+      #   let(:auth_token) { encode_token({ user_id: user.id }) }
+
+      #   let(:Authorization) { "Bearer #{auth_token}" }
+
+      #   let(:doctor) do
+      #     doctor = doc1.update(active: false)
+      #   end
+
+      #   # after do |example|
+      #   #   example.metadata[:response][:content] = {
+      #   #     'application/json' => {
+      #   #       example: JSON.parse(response.body, symbolize_names: true)
+      #   #     }
+      #   #   }
+      #   # end
+
+      #   run_test!
+      # end
+
+      # response '422', 'invalid request' do
+      #   # def encode_token(payload, exp = 7.days.from_now)
+      #   #   payload[:exp] = exp.to_i
+      #   #   JWT.encode(payload, 'mysecret')
+      #   # end
+
+      #   # let(:user) do
+      #   #   User.create(name: "John Smith", username: "smithey", email: "john@smithey.com", password: "smithey123", role: "admin")
+      #   # end
+
+      #   # let(:auth_token) { encode_token({ user_id: user.id }) }
+
+      #   # let(:Authorization) { 'Bearer ' + auth_token }
+
+      #   # let(:doctor) do
+      #   #   Doctor.create(name: 'Mike Venom', bio: 'Lorem ipsum is placeholder',
+      #   #                              photo: 'https://img.freepik.com/free-photo/world-doctors-day-thinking-blonde-young-doctor-with-stethoscope-lab-coat_140725-162409.jpg?', available_days: 'Monday - Frinday', specialities: 'Primary Care Doctor', hospital: 'NYU Langone Health', start_time: '08: 00 AM', end_time: '20: 00 PM', email: 'doc.mikevenom@apple.com', phone: "0981232323")
+      #   # end
+      #   # let(:id) { doctor.id }
+      #   after do |example|
+      #     example.metadata[:response][:content] = {
+      #       'application/json' => {
+      #         example: JSON.parse(response.body, symbolize_names: true)
+      #       }
+      #     }
+      #   end
+      #   run_test!
+      # end
     end
   end
 end
