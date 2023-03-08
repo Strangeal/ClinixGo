6786019c149707e03bdefea0bf648c3d242bf703
strangeal <bajerijustice@gmail.com>
2023-03-08 04:26:28 -0800

    Add base url for swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -22,10 +22,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://{defaultHost}',
+          url: 'https://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:3000'
+              default: 'clinixgo.up.railway.app'
             }
           }
         }
