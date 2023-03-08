f3a5a3654a50be01e9aa62de099b56f34efa0bd7
nahnahAJ <nahnahahjuwah@gmail.com>
2023-03-08 10:45:40 +0000

    Removed npm commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,7 @@
 set -o errexit
 
 bundle install
-npm install
-npm run build
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
+# bundle exec rake db:seed
